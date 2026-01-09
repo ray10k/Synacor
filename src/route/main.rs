506d946c66df87d31decf733bd9dbb584cf6cf6a
Vera <ray10k@xmsnet.nl>
2026-01-09 21:15:34 +0100

    More work on finding the route

--- src/route/main.rs
+++ src/route/main.rs
@@ -4,38 +4,28 @@ use room_map::*;
 
 fn main() {
     println!("hello world! Let's find the route to that treasure!");
-    let mut unchecked:Vec<Route> = Vec::with_capacity(4096);
-    let mut best = Route::worst_route();
-    unchecked.push(Route::new());
+    let mut unchecked:Vec<VecRoute> = Vec::with_capacity(4096);
+    let mut finished:Vec<VecRoute> = Vec::new();
+    unchecked.push(VecRoute::new());
     while !unchecked.is_empty() {
-        let mut start = unchecked.pop().expect("Somehow, the unchecked queue was both not empty and could not be popped from.");
+        let start = unchecked.pop().expect("Somehow, the unchecked queue was both not empty and could not be popped from.");
         if !start.verify() {
             continue;//route is *definitely* not viable, go do something else.
         }
-        let (start_x,start_y) = start.coordinates();
-        println!("from {start_x},{start_y}.");
-        for _ in start_y..=3 {
-            unchecked.push(start.add_step(StepDirection::West).unwrap_or(Route::worst_route()));
-            unchecked.push(start.add_step(StepDirection::South).unwrap_or(Route::worst_route()));
-            unchecked.push(start.add_step(StepDirection::East).unwrap_or(Route::worst_route()));
-            if let Some(next_step) = start.add_step(StepDirection::North) {
-                start = next_step;
-            }
+        let (start_x, start_y) = start.coordinates().expect("Valid route somehow went out of range.");
+        //println!("from {start_x},{start_y}.");
+
+        if start_x == 3 && start_y == 3 && start.orb_weight() == 30{
+            finished.push(start);
         }
-        for _ in start_x..=3 {
-            unchecked.push(start.add_step(StepDirection::North).unwrap_or(Route::worst_route()));
-            unchecked.push(start.add_step(StepDirection::West).unwrap_or(Route::worst_route()));
-            unchecked.push(start.add_step(StepDirection::South).unwrap_or(Route::worst_route()));
-            if let Some(next_step) = start.add_step(StepDirection::East) {
-                start = next_step;
-            }
-        }
-        //At this point, the orb should be at (3,3), the exit.
-        let final_weight = start.orb_weight();
-        //if the weight is exactly 30, this is a valid route.
-        if final_weight == 30 && start.step_count < best.step_count {
-            best = start;
+        else {
+            unchecked.push(start.add_step(StepDirection::North));
+            unchecked.push(start.add_step(StepDirection::East));
+            unchecked.push(start.add_step(StepDirection::South));
+            unchecked.push(start.add_step(StepDirection::West));
         }
     }
-    print!("{best}");
+    finished.sort_by(|a, b| a.len().cmp(&b.len()));
+    println!("Shortest route: {}",finished.last().expect("Zero routes found, somehow."));
+    println!("Longest route: {}",finished.first().expect("Zero routes found, somehow."));
 }
--- src/route/room_map.rs
+++ src/route/room_map.rs
@@ -174,3 +174,115 @@ impl Route {
         manhattan_distance + self.step_count < 16
     }
 }
+
+#[derive(Clone)]
+pub struct VecRoute(Vec<StepDirection>);
+
+impl VecRoute {
+    pub fn new() -> Self {
+        Self(Vec::new())
+    }
+
+    pub fn add_step(&self, step:StepDirection) -> Self{
+        let mut retval = self.clone();
+        retval.0.push(step);
+        retval
+    }
+
+    pub fn coordinates(&self) -> Option<(i8,i8)> {
+        let mut x = 0;
+        let mut y = 0;
+        for step in self.0.iter() {
+            match step {
+                StepDirection::North => y += 1,
+                StepDirection::East => x += 1,
+                StepDirection::South => y -= 1,
+                StepDirection::West => x -= 1,
+                StepDirection::None => (),
+            }
+            if x < 0 || x > 3 || y < 0 || y > 3 {
+                return None // if the path ever goes outside the playfield, 
+            } //the route has no determinable end-location since it is invalid.
+        }
+        Some((x,y))
+    }
+
+    pub fn orb_weight(&self) -> usize {
+        let mut retval = 22;
+        let mut x = 0;
+        let mut y = 0;
+        let mut last_op = TileContent::Value(22);
+        let map = generate_map();
+
+        for step in self.0.iter() {
+            match step {
+                StepDirection::North => {
+                    if y == 3 {
+                        return 0;
+                    }
+                    y += 1},
+                StepDirection::East => {
+                    if x == 3 {
+                        return 0;
+                    }
+                    x += 1},
+                StepDirection::South => {
+                    if y == 0 {
+                        return 0;
+                    }
+                    y -= 1;
+                },
+                StepDirection::West => {
+                    if x == 0 {
+                        return 0;
+                    }
+                    x -= 1},
+                StepDirection::None => (),
+            }
+            let index = y + (x * 4);
+            let curr_op = map[index];
+            if let TileContent::Value(value) = curr_op {
+                match last_op {
+                    TileContent::Plus => retval += value as usize,
+                    TileContent::Minus => {
+                        if value as usize >= retval {
+                            return 0;
+                        }
+                        retval -= value as usize;
+                    },
+                    TileContent::Asterisk => retval *= value as usize,
+                    TileContent::Value(_) => (), //just do nothing, since this will trigger on the initial 0,0 coordinate.
+                }
+            }
+            last_op = curr_op;
+        }
+        return retval;
+    }
+
+    pub fn verify(&self) -> bool {
+        let weight = self.orb_weight();
+        if weight == 0 {
+            return false;
+        }
+        if let None = self.coordinates() {
+            return false;
+        }
+        if self.0.len() > 16 { //excessive, but I want to check something.
+            return false;
+        }
+        return true;
+    }
+
+    pub fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+impl Display for VecRoute {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for step in self.0.iter() {
+            let _ = write!(f,"{step}\n")?;
+        }
+        std::fmt::Result::Ok(())
+    }
+}
