a02002f9317b2e14a1d6ce6c3c61f1d58eea7420
Vera <ray10k@xmsnet.nl>
2026-02-01 20:45:30 +0100

    Final puzzle solved. The challenge is now completed!

--- src/route/main.rs
+++ src/route/main.rs
@@ -6,21 +6,22 @@ use std::thread::{spawn, JoinHandle, available_parallelism};
 use std::sync::mpsc::channel;
 use std::time::Duration;
 
+const MAX_SEARCH_DEPTH:usize = 16;
+
 fn main() {
     println!("hello world! Let's find the route to that treasure!");
 
     let (main_sender,main_receiver) = channel::<VecRoute>();
     let shared_receiver = Arc::new(Mutex::new(main_receiver));
     let core_count:usize = available_parallelism().expect("Could not determine the number of cores.").into();
-    let mut thread_pool:Vec<JoinHandle<()>> = Vec::with_capacity(core_count);
-    let results= Arc::new(Mutex::new(Vec::new()));
+    let mut thread_pool:Vec<JoinHandle<Vec<VecRoute>>> = Vec::with_capacity(core_count);
     main_sender.send(VecRoute::new()).expect("Could not send initial route.");
 
     for _ in 0..core_count {
         let thread_sender = main_sender.clone();
         let thread_receiver = shared_receiver.clone();
-        let result_clone = results.clone();
         thread_pool.push(spawn(move || {
+            let mut local_results = Vec::new();
             loop {
                 let new_item = {
                     thread_receiver.lock().expect("Could not lock.").recv_timeout(Duration::from_secs(1))
@@ -29,34 +30,37 @@ fn main() {
                     Ok(start) => start,
                     Err(_) => break,
                 };
-                if !new_item.verify() {
+                if !new_item.verify(MAX_SEARCH_DEPTH) {
                     continue
                 }
                 let (start_x, start_y) = match new_item.coordinates() {
                     Some((x,y)) => (x,y),
                     None => continue,
                 };
-                if start_x == 0 && start_y == 0 && new_item.len() > 0 {
+                if start_x == 0 && start_y == 0 && new_item.len() > 0 { //Any route ends upon revisiting south-west.
                     continue
                 }
-                else if start_x == 3 && start_y == 3 && new_item.orb_weight() == 30 {
-                    result_clone.lock().expect("Could not lock results.").push(new_item);
+                else if start_x == 3 && start_y == 3 { //Any route ends upon first visit to north-east
+                    if new_item.orb_weight() == 30  {
+                        local_results.push(new_item);
+                    }
+                    continue
                 } else {
                     thread_sender.send(new_item.add_step(StepDirection::North)).expect("Could not send new North route.");
                     thread_sender.send(new_item.add_step(StepDirection::East)).expect("Could not send new East route.");
                     thread_sender.send(new_item.add_step(StepDirection::South)).expect("Could not send new South route.");
                     thread_sender.send(new_item.add_step(StepDirection::West)).expect("Could not send new West route.");
-
                 }
             }
+            return local_results;
         }))
     }
+    let mut finished = Vec::new();
     
     for handle in thread_pool.into_iter() {
-        handle.join().expect("Something went wrong joining the thread.");
+        let mut result = handle.join().expect("Something went wrong joining the thread.");
+        finished.append(&mut result);
     }
-
-    let mut finished = results.lock().expect("Could not lock results").clone();
     finished.sort_by(|a, b| a.len().cmp(&b.len()));
     println!("Shortest route: {}",finished.last().expect("Zero routes found, somehow."));
     println!("Longest route: {}",finished.first().expect("Zero routes found, somehow."));
--- src/route/room_map.rs
+++ src/route/room_map.rs
@@ -36,8 +36,7 @@ pub enum StepDirection {
     North,
     East,
     South,
-    West,
-    None
+    West
 }
 
 impl Display for StepDirection {
@@ -47,131 +46,9 @@ impl Display for StepDirection {
                 StepDirection::North => "north",
                 StepDirection::East => "east",
                 StepDirection::South => "south",
-                StepDirection::West => "west",
-                StepDirection::None => " ",
+                StepDirection::West => "west"
             }
         )
-    }
-}
-
-#[derive(Clone, Copy)]
-pub struct Route {
-    steps:[StepDirection; 15],
-    pub step_count:u8
-}
-
-impl Display for Route {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,"(Route with {} steps.)\n",self.step_count)?;
-        for i in 0..self.step_count {
-            write!(f,"{}\n",self.steps[i as usize])?;
-        }
-        std::fmt::Result::Ok(())
-    }
-}
-
-impl Route {
-    pub fn new() -> Self {
-        Route{
-            steps : [StepDirection::None;15],
-            step_count : 0
-        }
-    }
-
-    pub fn add_step(&self, step:StepDirection) -> Option<Self> {
-        if self.step_count >= 15 {
-            return None;
-        }
-        let mut retval = self.clone();
-        retval.step_count += 1;
-        retval.steps[self.step_count as usize] = step;
-        Some(retval)
-    }
-
-    pub fn worst_route() -> Self {
-        Route { steps: [StepDirection::None;15], step_count: 17 }
-    }
-
-    pub fn coordinates(&self) -> (u8,u8) {
-        let mut x = 0;
-        let mut y = 0;
-
-        for step in self.steps.iter() {
-            match step {
-                StepDirection::North => y += 1,
-                StepDirection::East => x += 1,
-                StepDirection::South => y -= 1,
-                StepDirection::West => x -= 1,
-                StepDirection::None => (),
-            }
-        }
-
-        (x,y)
-    }
-
-    pub fn orb_weight(&self) -> usize {
-        let mut retval = 22;
-        let mut x=0;
-        let mut y =0;
-        let mut last_op = TileContent::Value(22);
-        let map = generate_map();
-
-        for step in self.steps.iter() {
-            match step {
-                StepDirection::North => {
-                    if y == 3 {
-                        return 0;
-                    }
-                    y += 1},
-                StepDirection::East => {
-                    if x == 3 {
-                        return 0;
-                    }
-                    x += 1},
-                StepDirection::South => {
-                    if y == 0 {
-                        return 0;
-                    }
-                    y -= 1;
-                },
-                StepDirection::West => {
-                    if x == 0 {
-                        return 0;
-                    }
-                    x -= 1},
-                StepDirection::None => (),
-            }
-            let index = y + (x * 4);
-            let curr_op = map[index];
-            if let TileContent::Value(value) = curr_op {
-                match last_op {
-                    TileContent::Plus => retval += value as usize,
-                    TileContent::Minus => {
-                        if value as usize >= retval {
-                            return 0;
-                        }
-                        retval -= value as usize;
-                    },
-                    TileContent::Asterisk => retval *= value as usize,
-                    TileContent::Value(_) => (), //just do nothing, since this will trigger on the initial 0,0 coordinate.
-                }
-            }
-            last_op = curr_op;
-        }
-
-        return retval
-    }
-
-    pub fn verify(&self) -> bool {
-
-        let weight = self.orb_weight();
-        if weight == 0 {
-            return false;
-        }
-
-        let (x, y) = self.coordinates();
-        let manhattan_distance = (3 - x) + (3 - y);
-        manhattan_distance + self.step_count < 16
     }
 }
 
@@ -197,8 +74,7 @@ impl VecRoute {
                 StepDirection::North => y += 1,
                 StepDirection::East => x += 1,
                 StepDirection::South => y -= 1,
-                StepDirection::West => x -= 1,
-                StepDirection::None => (),
+                StepDirection::West => x -= 1
             }
             if x < 0 || x > 3 || y < 0 || y > 3 {
                 return None // if the path ever goes outside the playfield, 
@@ -236,8 +112,7 @@ impl VecRoute {
                     if x == 0 {
                         return 0;
                     }
-                    x -= 1},
-                StepDirection::None => (),
+                    x -= 1}
             }
             let index = y + (x * 4);
             let curr_op = map[index];
@@ -259,7 +134,7 @@ impl VecRoute {
         return retval;
     }
 
-    pub fn verify(&self) -> bool {
+    pub fn verify(&self,max_depth:usize) -> bool {
         let weight = self.orb_weight();
         if weight == 0 {
             return false;
@@ -267,7 +142,7 @@ impl VecRoute {
         if let None = self.coordinates() {
             return false;
         }
-        if self.0.len() > 16 { //excessive, but I want to check something.
+        if self.0.len() > max_depth { //excessive, but I want to check something.
             return false;
         }
         return true;
